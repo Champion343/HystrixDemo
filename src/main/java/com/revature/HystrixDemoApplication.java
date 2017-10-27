a81b555b47bc03459a6c1dca186f8236db3d1eff
jackson.demaree <jackson.demaree@tamu.edu>
2017-10-27 09:36:43 -0400

    added swagger2

--- src/main/java/com/revature/HystrixDemoApplication.java
+++ src/main/java/com/revature/HystrixDemoApplication.java
@@ -1,13 +1,21 @@
 package com.revature;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 
@@ -16,6 +24,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 @EnableHystrix
 @RestController
 @EnableDiscoveryClient
+@EnableSwagger2
 public class HystrixDemoApplication {
 
 	public static void main(String[] args) {
@@ -27,4 +36,14 @@ public class HystrixDemoApplication {
 	public String helloEndpoint() {
 		return "hello";
 	}
+	
+	
+	@Bean
+	public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().paths(regex("/.*"))
+                .build();
+             
+    }
+	
 }
